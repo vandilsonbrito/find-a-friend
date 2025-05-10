c0a6e07c8b47db3441a467cd88f4a94d96a7b37c
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-10 20:15:13 -0300

    feat: create InMemoryOrgsRepository

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,34 @@
+import { Org, Prisma } from '@prisma/client'
+import { IOrgsRepository } from '../orgs-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryOrgsRepository implements IOrgsRepository {
+  public items: Org[] = []
+
+  async findEmail(email: string) {
+    const org = this.items.find((item) => item.email === email)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+
+  async create(data: Prisma.OrgCreateInput) {
+    const org = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      whatsapp: data.whatsapp,
+      address: data.address,
+      city: data.city,
+      created_at: new Date(),
+    }
+
+    this.items.push(org)
+
+    return org
+  }
+}
