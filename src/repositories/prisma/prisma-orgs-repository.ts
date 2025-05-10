76707af59ef46652c7fe1ba0e5848babee3c9293
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-10 20:15:39 -0300

    feat: create PrismaOrgsRepository

---
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -0,0 +1,21 @@
+import { prisma } from 'lib/prisma'
+import { IOrgsRepository } from '../orgs-repository'
+import { Prisma } from '@prisma/client'
+
+export class PrismaOrgsRepository implements IOrgsRepository {
+  async findEmail(email: string) {
+    const org = await prisma.org.findUnique({
+      where: { email },
+    })
+
+    return org
+  }
+
+  async create(data: Prisma.OrgCreateInput) {
+    const org = await prisma.org.create({
+      data,
+    })
+
+    return org
+  }
+}
