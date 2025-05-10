d6aebb27f7342d4e085e50badf61fdec604f9418
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-10 20:11:45 -0300

    feat: register ogrsRoutes and add global setErrorHandler on app to format error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
 import fastify from 'fastify'
-//import { PrismaClient } from '@prisma/client'
+import { orgsRoutes } from './http/controllers/orgs/routes'
+import { env } from './env'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
-//const prisma = new PrismaClient()
+app.register(orgsRoutes)
 
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+})
