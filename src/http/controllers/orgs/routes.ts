e5f83b2c9fe95e0d5495024f244de4e609b5a15d
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-12 11:49:01 -0300

    feat: register routes for authenticate and refresh token

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,6 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
+import { refresh } from './refresh'
+import { authenticate } from './authenticate'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/org', register)
+  app.post('/authenticate', authenticate)
+
+  app.patch('/token/refresh', refresh)
 }
