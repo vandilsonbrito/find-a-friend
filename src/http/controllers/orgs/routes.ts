74ef1dc0d7f3120dca237cf74e45eaa9c3b2f677
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-20 09:06:53 -0300

    feat: declare '/me' route for 'orgProfile' controller

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -2,10 +2,15 @@ import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { refresh } from './refresh'
 import { authenticate } from './authenticate'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { orgProfile } from './profile'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/org', register)
   app.post('/authenticate', authenticate)
 
   app.patch('/token/refresh', refresh)
+
+  /* Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, orgProfile)
 }
