57cdd3af910775432902fcf5aa71a42a71459558
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-20 09:01:16 -0300

    test: create tests for GetOrgProfileUseCase

---
+++ src/use-cases/orgs/get-org-profile.spec.ts
@@ -0,0 +1,41 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
+import { GetOrgProfileUseCase } from './get-org-profile'
+import { hash } from 'bcryptjs'
+import { OrgNotFoundError } from '../errors/org-not-found-error'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: GetOrgProfileUseCase
+
+describe('Get Org Profile Use Case', () => {
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new GetOrgProfileUseCase(orgsRepository)
+  })
+
+  it('should be able to get Organization profile', async () => {
+    const createdOrg = await orgsRepository.create({
+      name: 'John Doe',
+      email: 'Z6m4I@example.com',
+      password_hash: await hash('123456', 6),
+      whatsapp: '11999999999',
+      address: 'Rua Teste',
+      city: 'São Paulo',
+      created_at: new Date(),
+    })
+
+    const { org } = await sut.execute({
+      orgId: createdOrg.id,
+    })
+    console.log('Org', org)
+    expect(org.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to get Organization profile with wrong id', async () => {
+    await expect(() => {
+      return sut.execute({
+        orgId: 'non-existing-id',
+      })
+    }).rejects.toBeInstanceOf(OrgNotFoundError)
+  })
+})
