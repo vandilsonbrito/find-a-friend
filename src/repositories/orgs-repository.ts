c2f1c261c194e03ddbbf9a36336c6f6ce5282fdd
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-20 08:53:26 -0300

    feat: add findById method interface

--- src/repositories/orgs-repository.ts
+++ src/repositories/orgs-repository.ts
@@ -3,4 +3,5 @@ import { Org, Prisma } from '@prisma/client'
 export interface IOrgsRepository {
   findEmail(email: string): Promise<Org | null>
   create(data: Prisma.OrgCreateInput): Promise<Org>
+  findById(orgId: string): Promise<Org | null>
 }
