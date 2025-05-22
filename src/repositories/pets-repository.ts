1922ab14bb59a4d36c69f3a14675e8dd7c29b3d4
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-22 09:31:37 -0300

    feat: declare `save` and `delete`methods

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -2,6 +2,8 @@ import { GetPetsAvailableForAdoptionUseCaseRequest } from '@/@types/get-pets-ava
 import { Pet, Prisma } from '@prisma/client'
 
 export interface IPetsRepository {
+  delete(petId: string): Promise<void>
+  save(pet: Pet): Promise<Pet>
   findById(petId: string): Promise<Pet | null>
   create(data: Prisma.PetCreateInput): Promise<Pet>
   findManyByCityAndFilters(
