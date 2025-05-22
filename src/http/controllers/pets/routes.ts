147badaaeb35b80648e3acb7dea6703bc03311a5
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-21 22:26:09 -0300

    feat: add `get - /pets` route and add jwt verification on `post - /pets` pets creation

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { createPet } from './create'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { getAvailablePetsForAdoption } from './get-available-for-adoption'
 export async function petRoutes(app: FastifyInstance) {
-  app.post('/pets', createPet)
+  app.post('/pets', { onRequest: [verifyJWT] }, createPet)
+  app.get('/pets', { onRequest: [verifyJWT] }, getAvailablePetsForAdoption)
 }
