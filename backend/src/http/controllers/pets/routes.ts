eec4850aeee6fb31d44b1bcb209a13eeef17e4b9
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-05-28 21:12:22 -0300

    refactor: remove auth for `getAvailablePetsForAdoption` controller

--- backend/src/http/controllers/pets/routes.ts
+++ backend/src/http/controllers/pets/routes.ts
@@ -5,8 +5,9 @@ import { getAvailablePetsForAdoption } from './get-available-for-adoption'
 import { editPet } from './edit'
 import { removePet } from './remove'
 export async function petRoutes(app: FastifyInstance) {
+  app.get('/pets', getAvailablePetsForAdoption)
+
   app.post('/pets', { onRequest: [verifyJWT] }, createPet)
-  app.get('/pets', { onRequest: [verifyJWT] }, getAvailablePetsForAdoption)
   app.put('/pets/:petId', { onRequest: [verifyJWT] }, editPet)
   app.delete('/pets/:petId', { onRequest: [verifyJWT] }, removePet)
 }
