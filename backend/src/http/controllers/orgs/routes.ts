9b1c1759f0400b99bd9025ed21c5402749e20506
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-06-02 18:11:35 -0300

    feat: declare routes for `allOrgs` and `editOrg` controllers

--- backend/src/http/controllers/orgs/routes.ts
+++ backend/src/http/controllers/orgs/routes.ts
@@ -5,12 +5,16 @@ import { authenticate } from './authenticate'
 import { verifyJWT } from '../../../http/middlewares/verify-jwt'
 import { orgProfile } from './profile'
 import { logout } from './logout'
+import { editOrg } from './edit'
+import { allOrgs } from './get-all'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', register)
   app.post('/sessions', authenticate)
   app.patch('/token/refresh', refresh)
+  app.get('/orgs', allOrgs)
 
   app.get('/me', { onRequest: [verifyJWT] }, orgProfile)
+  app.put('/orgs/:orgId/profile', { onRequest: [verifyJWT] }, editOrg)
   app.post('/logout', { onRequest: [verifyJWT] }, logout)
 }
