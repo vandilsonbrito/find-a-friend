bd64ee31296d0b0c0b39a004a60ffcfedf0e35d4
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-06-02 22:19:07 -0300

    feat: implement on prisma repository `findAll` and `editOrg` methods

--- backend/src/repositories/prisma/prisma-orgs-repository.ts
+++ backend/src/repositories/prisma/prisma-orgs-repository.ts
@@ -3,6 +3,11 @@ import { IOrgsRepository } from '../orgs-repository'
 import { Prisma } from '@prisma/client'
 
 export class PrismaOrgsRepository implements IOrgsRepository {
+  async findAll() {
+    const orgs = await prisma.org.findMany()
+
+    return orgs
+  }
   async findEmail(email: string) {
     const org = await prisma.org.findUnique({
       where: { email },
@@ -13,6 +18,15 @@ export class PrismaOrgsRepository implements IOrgsRepository {
 
   async create(data: Prisma.OrgCreateInput) {
     const org = await prisma.org.create({
+      data,
+    })
+
+    return org
+  }
+
+  async edit(data: Prisma.OrgUpdateInput) {
+    const org = await prisma.org.update({
+      where: { id: data.id as string },
       data,
     })
 
