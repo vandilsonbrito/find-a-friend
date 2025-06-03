cd0fbf83e03b3df0f756fa57c3a1e02d8efeb6fa
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-06-02 22:14:33 -0300

    feat: declare route for `getOrgPets` controller

--- backend/src/http/controllers/pets/routes.ts
+++ backend/src/http/controllers/pets/routes.ts
@@ -4,9 +4,11 @@ import { verifyJWT } from '../../../http/middlewares/verify-jwt'
 import { getAvailablePetsForAdoption } from './get-available-for-adoption'
 import { editPet } from './edit'
 import { removePet } from './remove'
+import { getOrgPets } from './org-pets'
 export async function petRoutes(app: FastifyInstance) {
   app.get('/pets', getAvailablePetsForAdoption)
 
+  app.get('/orgs/:orgId/pets', { onRequest: [verifyJWT] }, getOrgPets)
   app.post('/pets', { onRequest: [verifyJWT] }, createPet)
   app.put('/pets/:petId', { onRequest: [verifyJWT] }, editPet)
   app.delete('/pets/:petId', { onRequest: [verifyJWT] }, removePet)
