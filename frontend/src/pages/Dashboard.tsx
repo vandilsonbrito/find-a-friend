5e4379691ea9572baebe37b60c9c9542bf7a5dbb
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-06-03 17:48:18 -0300

    feat: create integration with services and add pet card skeleton loading as fallback

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,87 +1,159 @@
-import React, { useState } from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs'
 import Navbar from '../components/layout/NavBar'
 import Footer from '../components/layout/Footer'
 import StatsCards from '../components/dashboard/StatsCards'
 import PetCard from '../components/dashboard/PetCard'
-import AddPetDialog, { type NewPet } from '../components/dashboard/AddPetDialog'
+import AddPetDialog, { type PetType } from '../components/dashboard/AddPetDialog'
 import OrganizationProfile from '../components/dashboard/OrganizationProfile'
 import EmptyPetsState from '../components/dashboard/EmptyPetsState'
+import useAuthContext from '../hooks/useAuthContext'
+import { ErrorToast } from '../components/ErrorToast'
+import { SuccessToast } from '../components/SuccessToast'
+import { useGetOrgPets } from '../services/hooks/useGetOrgPets'
+import PetSkeletonCardShimmer from '../components/dashboard/SkeletonLoadingPetCard'
+import StatsSkeleton from '../components/dashboard/SkeletonLoadingStatsCard'
+import { petService } from '../services/petService'
+import type { AgePT, EnergyLevelPT, EnvironmentPT, IndependenceLevelPT, SexPT, SizePT, TypePT } from '../@types'
 
-const mockPets = [
-  {
-    id: '1',
-    name: 'Max',
-    type: 'Cachorro',
-    breed: 'Golden Retriever',
-    age: '2 anos',
-    size: 'Grande',
-    gender: 'Macho',
-    status: 'Disponível',
-    description: 'Max é um cão muito carinhoso e brincalhão.',
-    photos: ['/placeholder.svg'],
-    createdAt: '2024-01-15',
-  },
-  {
-    id: '2',
-    name: 'Luna',
-    type: 'Gato',
-    breed: 'Siamês',
-    age: '1 ano',
-    size: 'Pequeno',
-    gender: 'Fêmea',
-    status: 'Adotado',
-    description: 'Luna é uma gatinha muito carinhosa.',
-    photos: ['/placeholder.svg'],
-    createdAt: '2024-02-01',
-  },
-]
+export interface NewPet {
+  name: string
+  description: string
+  age: AgePT
+  size: SizePT
+  sex: SexPT
+  energy_level: EnergyLevelPT
+  independence_level: IndependenceLevelPT
+  environment: EnvironmentPT
+  type: TypePT
+  breed: string
+  city: string
+  state: string
+  photos: string[]
+  is_adopted: boolean
+}
 
-const Dashboard: React.FC = () => {
-  const [pets, setPets] = useState(mockPets)
+const Dashboard = () => {
+  const { user } = useAuthContext()
+
+  const {
+    data: petsData,
+    refetch,
+    isError,
+    isFetching,
+  } = useGetOrgPets(user?.id as string)
+
   const [isAddingPet, setIsAddingPet] = useState(false)
+  const [isAddingPetLoading, setIsAddingPetLoading] = useState(false)
+  const [selectedPetIdToEdit, setSelectedPetIdToEdit] = useState('')
+
   const [newPet, setNewPet] = useState<NewPet>({
     name: '',
-    type: '',
-    breed: '',
-    age: '',
-    size: '',
-    gender: '',
     description: '',
+    age: 'Filhote',
+    size: 'Pequeno',
+    sex: 'Macho',
+    energy_level: 'Baixo',
+    independence_level: 'Baixo',
+    environment: 'Pequeno',
+    type: 'Cachorro',
+    breed: '',
+    city: '',
+    state: '',
     photos: [],
+    is_adopted: false,
   })
 
-  const handleAddPet = () => {
-    const pet = {
-      id: Date.now().toString(),
-      ...newPet,
-      status: 'Disponível',
-      photos: ['/placeholder.svg'],
-      createdAt: new Date().toISOString().split('T')[0],
-      views: 0,
-      favorites: 0,
+  const handleAddPet = async (): Promise<boolean> => {
+    try {
+      setIsAddingPetLoading(true)
+      const response = await petService.addPet(newPet, user?.id as string)
+      if (response.status === 201) {
+        SuccessToast('Pet adicionado com sucesso!')
+        refetch()
+        resetForm()
+        return true
+      } else {
+        ErrorToast('Erro ao adicionar pet.')
+        return false
+      }
+    } catch (error) {
+      ErrorToast('Erro ao adicionar pet.')
+      console.error(error)
+      return false
+    } finally {
+      setIsAddingPetLoading(false)
     }
-    setPets([...pets, pet])
+  }
+
+  const handleEditPet = async (petId: string, pet: PetType): Promise<boolean> => {
+    try {
+      const response = await petService.editPet(petId, pet)
+      if (response.status === 200) {
+        SuccessToast('Pet editado com sucesso!')
+        refetch()
+        setIsAddingPet(false)
+        return true
+      } else {
+        ErrorToast('Erro ao editar pet.')
+        return false
+      }
+    } catch (error) {
+      ErrorToast('Erro ao editar pet.')
+      console.error(error)
+      return false
+    }
+  }
+
+  const handleDeletePet = async (petId: string) => {
+    try {
+      await petService.deletePet(petId)
+      SuccessToast('Pet excluído com sucesso!')
+      refetch()
+    } catch (error) {
+      ErrorToast('Erro ao excluir pet.')
+      console.error(error)
+    }
+  }
+
+  const resetForm = () => {
     setNewPet({
       name: '',
-      type: '',
-      breed: '',
-      age: '',
-      size: '',
-      gender: '',
       description: '',
+      age: 'Filhote',
+      size: 'Pequeno',
+      sex: 'Macho',
+      energy_level: 'Baixo',
+      independence_level: 'Baixo',
+      environment: 'Pequeno',
+      type: 'Cachorro',
+      breed: '',
+      city: '',
+      state: '',
       photos: [],
+      is_adopted: false,
     })
     setIsAddingPet(false)
+    setSelectedPetIdToEdit('')
   }
 
-  const handleDeletePet = (petId: string) => {
-    setPets(pets.filter((pet) => pet.id !== petId))
-  }
+  const totalPets = petsData?.length || 0
+  const availablePets = petsData?.filter((pet) => !pet.is_adopted).length || 0
+  const adoptedPets = petsData?.filter((pet) => pet.is_adopted).length || 0
 
-  const totalPets = pets.length
-  const availablePets = pets.filter((pet) => pet.status === 'Disponível').length
-  const adoptedPets = pets.filter((pet) => pet.status === 'Adotado').length
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  if (isError) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p>Erro ao carregar pets</p>
+      </div>
+    )
+  }
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
@@ -93,11 +165,14 @@ const Dashboard: React.FC = () => {
             <p className="text-foreground/70">Gerencie seus pets para adoção</p>
           </div>
 
-          <StatsCards
-            totalPets={totalPets}
-            availablePets={availablePets}
-            adoptedPets={adoptedPets}
-          />
+          {isFetching && <StatsSkeleton />}
+          {petsData && (
+            <StatsCards
+              totalPets={totalPets}
+              availablePets={availablePets}
+              adoptedPets={adoptedPets}
+            />
+          )}
 
           <Tabs defaultValue="pets" className="space-y-6">
             <TabsList>
@@ -108,22 +183,38 @@ const Dashboard: React.FC = () => {
             <TabsContent value="pets" className="space-y-6">
               <div className="flex justify-between items-center">
                 <h2 className="text-xl font-semibold">Pets Cadastrados</h2>
+
                 <AddPetDialog
                   isOpen={isAddingPet}
                   onOpenChange={setIsAddingPet}
                   newPet={newPet}
                   setNewPet={setNewPet}
                   onAddPet={handleAddPet}
+                  onEdit={handleEditPet}
+                  isaddingPetLoading={isAddingPetLoading}
+                  selectedPetIdToEdit={selectedPetIdToEdit}
+                  setSelectedPetIdToEdit={setSelectedPetIdToEdit}
                 />
               </div>
 
-              {pets.length > 0 ? (
+              {isFetching && (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                  {Array.from({ length: 8 }).map((_, index) => (
+                    <PetSkeletonCardShimmer key={index} />
+                  ))}
+                </div>
+              )}
+
+              {petsData && petsData.length > 0 ? (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                  {pets.map((pet) => (
+                  {petsData.map((pet) => (
                     <PetCard
                       key={pet.id}
                       pet={pet}
+                      orgId={user?.id as string}
                       onDelete={handleDeletePet}
+                      setIsAddingPet={setIsAddingPet}
+                      setSelectedPetIdToEdit={setSelectedPetIdToEdit}
                     />
                   ))}
                 </div>
