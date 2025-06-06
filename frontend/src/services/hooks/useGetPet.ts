5408d1568512901b60ae1f1dbd4287cc22cb8243
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-06-05 22:11:46 -0300

    feat: create `useGetPet` hook

---
+++ frontend/src/services/hooks/useGetPet.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import axiosInstance from '../../axios'
+import type { PetFromAPI } from '../../@types'
+
+type GetPetResponse = {
+  pets_data: {
+    pet: PetFromAPI
+  }
+}
+
+export function useGetPet(petId: string) {
+  async function getPet() {
+    const response = await axiosInstance.get<GetPetResponse>(`/pets/${petId}`)
+    const data  = response.data
+    return data || []
+  }
+
+  return useQuery({
+    queryKey: ['pet', petId],
+    queryFn: getPet,
+    enabled: !!petId,
+    refetchOnMount: true,
+  })
+}
