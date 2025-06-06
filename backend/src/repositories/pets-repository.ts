71c00f784d5321a9b3d8a4c2a93c9da9f6b3ca54
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-06-05 21:36:53 -0300

    refactor: update interface to return paginated pets and include PetWithPhoto in findById

--- backend/src/repositories/pets-repository.ts
+++ backend/src/repositories/pets-repository.ts
@@ -1,15 +1,19 @@
 import { GetPetsAvailableForAdoptionUseCaseRequest } from '../@types/get-pets-available-for-adoption-use-case'
 import { Pet, Prisma } from '@prisma/client'
+import { PetWithPhoto } from './prisma/prisma-pets-repository'
 
 export interface IPetsRepository {
   update(data: Prisma.PetUpdateInput): Promise<Pet>
   delete(petId: string): Promise<void>
   save(pet: Pet): Promise<Pet>
-  findById(petId: string): Promise<Pet | null>
+  findById(petId: string): Promise<PetWithPhoto | null>
   create(data: Prisma.PetCreateInput): Promise<Pet>
-  findAvailablePets(
-    data: GetPetsAvailableForAdoptionUseCaseRequest,
-  ): Promise<Pet[]>
+  findAvailablePets(data: GetPetsAvailableForAdoptionUseCaseRequest): Promise<{
+    pets: Pet[]
+    total_pets: number
+    current_page: number
+    total_pages: number
+  }>
   findManyByOrgId(orgId: string, page: number): Promise<Pet[]>
   countAvailablePetsByOrg(orgId: string): Promise<number>
 }
