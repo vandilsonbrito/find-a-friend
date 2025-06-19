93c951c68e9bbaaf806354dfa2b080a266b38e22
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-06-19 18:48:49 -0300

    refactor: api response path

--- frontend/src/services/hooks/useGetPet.ts
+++ frontend/src/services/hooks/useGetPet.ts
@@ -3,15 +3,13 @@ import axiosInstance from '../../axios'
 import type { PetFromAPI } from '../../@types'
 
 type GetPetResponse = {
-  pets_data: {
-    pet: PetFromAPI
-  }
+  pet: PetFromAPI
 }
 
 export function useGetPet(petId: string) {
   async function getPet() {
     const response = await axiosInstance.get<GetPetResponse>(`/pets/${petId}`)
-    const data  = response.data
+    const data = response.data
     return data || []
   }
 
